12f7bc58d5af5479baaed68e6d25132e45d6f2d3
Park-Han-Baek <totwjfakd@gmail.com>
2021-07-11 21:37:37 +0900

    auth page route and group page route

--- routes/control.js
+++ routes/control.js
@@ -6,9 +6,11 @@ const router = express.Router();
 const index = require('./index.js');
 const users = require('./users.js');
 const groups = require('./group.js');
+const auth = require('./auth.js');
 
 router.use('/', index);
 router.use('/users', users);
 router.use('/group', groups);
+router.use('/auth', auth);
 
 module.exports = router;
