ca7583a095c074785f25d7e139a76325aa50b8c6
Park-Han-Baek <totwjfakd@gmail.com>
2021-07-14 01:02:12 +0900

    로그인 구현, 세션 설정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { is_login: req.session.is_login });
 });
 
 module.exports = router;
