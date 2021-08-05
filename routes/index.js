e10536a35bf82d84550adea1cc015eef23c5ae56
Park-Han-Baek <totwjfakd@gmail.com>
2021-08-05 16:40:01 +0900

    유저 페이지 구현

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { isLogin: req.session.is_login });
+  res.render('index', { isLogin: req.session.is_login, userName : req.session.name, userEmail : req.session.email });
 });
 
 module.exports = router;
--- routes/mypage.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('mypage', { isLogin: req.session.is_login, userName : req.session.name, userId : req.session.id, userEmail : req.session.email });
-});
-
-module.exports = router;
