4cfd9916da2fffd399c41dff116721672b2e3f6d
Park-Han-Baek <totwjfakd@gmail.com>
2021-08-10 15:18:17 +0900

    유저 페이지 서버 구현(수정본)

--- routes/group.js
+++ routes/group.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var Group = require("../models/group");
-
+var User = require('../models/user');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
@@ -11,15 +11,29 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res) {
     var group = new Group();
+    
 
     group.groupName = req.body.groupName;
     group.groupExplanation = req.body.groupContents;
-
+    group.groupMember.push(req.session.userID)
+    User.findOne({userID : req.session.userID}, (err, user) => {
+        if (err) console.log(err)
+        else if (user) {
+            user.group.push(req.body.groupName)
+        }
+        else {
+            res.send('잘못된 접근 입니다.');
+        }
+    });
+    // 
     group.save(function(err) {
         if (err) {
             res.redirect('/group');
         }
-        res.redirect('/group');
+        else {
+            res.render('groupPage', {isLogin: req.session.is_login, userName : req.session.name, userEmail : req.session.email, userID : req.session.userID});
+        }
+        
     });
 });
 
--- routes/userpage.js
+++ routes/userpage.js
@@ -4,18 +4,21 @@ var User = require('../models/user');
 
 /* GET users listing. */
 router.get('/:userid', function(req, res, next) {
-
-  if (req.session.userID == req.params.userid) { // 로그인 되었을때
-    res.render('userpage', { isLogin: req.session.is_login, userName : req.session.name, userId : req.session.idcode, userEmail : req.session.email, userID : req.session.userID });
-  }
-  else {
-    User.findOne({userID : req.params.userid}, (err, user) => {
-      if (err) console.log(err);
-      else if (user) {
+  User.findOne({userID : req.params.userid}, (err, user) => {
+    if (err) console.log(err);
+    else if (user) {
+      if (req.session.userID == req.params.userid) { // 자신의 유저 페이지에 접속 했을때 수정 기능 추가 할 것
+        res.render('userpage', { isLogin: req.session.is_login, userName : req.session.name, userId : req.session.idcode, userEmail : req.session.email, userID : req.session.userID });
+      }
+      else { // 로그인안한 사용자 + 로그인 했지만 다른 사용자를 볼떄
         res.render('userpage', { isLogin: req.session.is_login, userName : user.name, userEmail : user.email});
       }
-    });
-  }
+    }
+    else {
+      res.send('유저를 찾을 수 없음');
+    }
+  });
+
   
 });
 
